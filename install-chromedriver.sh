8a60dcc0d1c771c040bc99ae60aacadd23a707bd
Evgen Matveev <mfb.eugene@gmail.com>
2017-08-03 11:31:08 +0300

    editor config added

--- install-chromedriver.sh
+++ install-chromedriver.sh
@@ -3,4 +3,3 @@ wget https://chromedriver.storage.googleapis.com/2.31/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 mv chromedriver Drivers
 rm chromedriver_linux64.zip
-
--- install-selenium.sh
+++ install-selenium.sh
@@ -2,4 +2,3 @@
 
 wget https://selenium-release.storage.googleapis.com/3.4/selenium-server-standalone-3.4.0.jar
 mv selenium-server-standalone-3.4.0.jar Drivers
-  
