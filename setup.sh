a05d2d4b91c907f4e1c4f15058960bd784371812
Evgen Matveev <mfb.eugene@gmail.com>
2017-08-03 11:52:40 +0300

    changed setup script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 rm -rf ./.git/
 
 # Make directory for drivers
-mkdir ./Drivers ./Logs ./Output
+mkdir ./Drivers ./Logs ./Logs/Selenium ./Output
 
 # Download selenium standalone server
 wget https://selenium-release.storage.googleapis.com/3.4/selenium-server-standalone-3.4.0.jar
