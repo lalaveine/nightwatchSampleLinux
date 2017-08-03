3d147fd00021650073804b48cbc1ddda13462228
Evgen Matveev <mfb.eugene@gmail.com>
2017-08-03 13:54:03 +0300

    added geckodriver

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-Sample nightwatch.js test environment for Linux
+**Sample nightwatch.js test environment for Linux**
 
 Chromedriver version: 2.31
+
+Geckodriver version: 0.18.0
+
 Selenium standalone server version: 3.4.0
+
 Nightwatch.js version: 0.9.16
 
-Requirements: nodejs and npm installed and updated
+_Requirements_: openjdk, nodejs and npm installed and updated
--- setup.sh
+++ setup.sh
@@ -13,8 +13,14 @@ mv ./selenium-server-standalone-3.4.0.jar ./Drivers
 # Install chromedriver
 wget https://chromedriver.storage.googleapis.com/2.31/chromedriver_linux64.zip
 unzip ./chromedriver_linux64.zip
-mv ./chromedriver Drivers
+mv ./chromedriver ./Drivers
 rm ./chromedriver_linux64.zip
+
+# Install geckodriver(firefox)
+wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz
+tar xzf "geckodriver-v0.18.0-linux64.tar.gz"
+mv ./geckodriver ./Drivers
+rm ./geckodriver-v0.18.0-linux64.tar.gz
 
 # Install nightwatch.js
 npm install
